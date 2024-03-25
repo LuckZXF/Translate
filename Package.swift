e1bc3fd6da899b99124157afbcb14fffb1d9b1fd
zhaoxifan <179988305@qq.com>
2024-03-25 18:16:33 +0800

    update

--- Package.swift
+++ Package.swift
@@ -4,12 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "TranslateCore",
+    name: "Translate",
     products: [
         .library(name: "TranslateCore", targets: ["TranslateCore"])
     ],
-    
+    dependencies: [
+        .package(
+            url: "https://github.com/IngmarStein/CommandLineKit",
+            from: "2.3.0"
+        )
+    ],
     targets: [
+        .target(
+            name: "Translate",
+            dependencies: ["TranslateCore","CommandLineKit"]
+        ),
         .target(
             name: "TranslateCore",
             dependencies: [],
